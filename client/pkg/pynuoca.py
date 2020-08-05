abfe0828c99ea7907b74ae47127a0c09736fd735
Martin Kysel <mkysel@nuodb.com>
2020-08-05 11:23:28 -0400

    use pip

--- client/pkg/pynuoca.py
+++ client/pkg/pynuoca.py
@@ -1,59 +1,52 @@
-# (C) Copyright NuoDB, Inc. 2019  All Rights Reserved.
+# (C) Copyright NuoDB, Inc. 2020  All Rights Reserved.
 #
-# Add the pynuoca module
+# Add the nuodb collection agent
 
 import os
 
 from client.package import Package
 from client.stage import Stage
-from client.artifact import GitHubMetadata, Artifact
+from client.artifact import PyPIMetadata, Artifact
 from client.utils import *
 
-class NuoCAPackage(Package):
-    """Add the NuoDB Collection Agent."""
+class PyNuoCA(Package):
+    """Add the NuoDB Collection Agent"""
 
     __PKGNAME = 'pynuoca'
 
-    __USER = 'nuodb'
-    __REPO = 'nuoca'
-    __ZIP = 'pynuoca.zip'
-
     def __init__(self):
-        super(NuoCAPackage, self).__init__(self.__PKGNAME)
-        self._zip = None
+        super(PyNuoCA, self).__init__(self.__PKGNAME)
+        self._file = None
 
         self.staged = [Stage(self.__PKGNAME,
                              title='NuoDB Collection Agent',
                              requirements='Python 2')]
+        # There's only one, make it simple
         self.stage = self.staged[0]
 
     def download(self):
-        repo = GitHubMetadata(self.__USER, self.__REPO)
-        self.stage.version = repo.version
+        # Find the latest release
+        pypi = PyPIMetadata(self.__PKGNAME)
 
-        self._zip = Artifact(self.name, self.__ZIP, repo.pkgurl)
-        self._zip.update()
+        self.setversion(pypi.version)
+
+        self._file = Artifact(self.name, 'pynuoca.tar.gz',
+                              pypi.pkgurl, chksum=pypi.pkgchksum)
+        self._file.update()
 
     def unpack(self):
         rmdir(self.pkgroot)
         mkdir(self.pkgroot)
-        unpack_file(self._zip.path, self.pkgroot)
-
-        for file_name in os.listdir(self.pkgroot):
-            self.stage.basedir = os.path.join(self.pkgroot, file_name)
+        unpack_file(self._file.path, self.pkgroot)
+        self.stage.basedir = os.path.join(self.pkgroot, 'pynuoca-{}'.format(self.stage.version))
 
     def install(self):
-        if Globals.target != 'lin64':
-            info('{}: Skipping on {} platform'.format(self.name, Globals.target))
-            return
-
-        self.stage.stage(self.__PKGNAME, ['src/'])
-
-        self.stage.stage('etc', ['etc/'])
+        self.stage.stage('python', ['pynuoca'])
         self.stage.stage('lib', ['lib/'])
-        self.stage.stage('plugins', ['plugins/'])
-
+        self.stage.stage('doc', ['README.rst', 'LICENSE'])
         self.stage.stage('bin', ['bin/'])
+        self.stage.stage('etc', ['etc/'])
+
 
 # Create and register this package
-NuoCAPackage()
+PyNuoCA()
