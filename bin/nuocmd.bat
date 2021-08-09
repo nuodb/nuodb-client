b5b0c74165617ef3e73c219517dcff22ededac5e
Paul Smith <paul@mad-scientist.net>
2021-08-09 13:16:01 -0400

    nuocmd.bat: Use the correct nuodb_cli module name

--- bin/nuocmd.bat
+++ bin/nuocmd.bat
@@ -1,6 +1,6 @@
 @echo off
 REM -- Invoke the NuoDB Admin command-line tool
-REM (C) Copyright 2019 NuoDB, Inc.  All Rights Reserved.
+REM (C) Copyright 2019-2021 NuoDB, Inc.  All Rights Reserved.
 
 set "PYTHONPATH=%~dp0..\etc\python\site-packages"
-python -m nuodb_cli.py %*
+python -m pynuoadmin.nuodb_cli %*
