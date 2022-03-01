25ea2979c1558c47d159150aa8200ae36d6d8668
Eusebio Resende <eresende@nuodb.com>
2022-03-01 09:36:20 -0500

    [QA-4090] Remove Hibernate 3 from Client Package build

--- client/pkg/hibernate.py
+++ client/pkg/hibernate.py
@@ -11,7 +11,7 @@ from client.utils import mkdir, rmdir, copy, savefile
 
 
 class HibernatePackage(Package):
-    """Add the NuoDB Hibernate (3 and 5) clients."""
+    """Add the NuoDB Hibernate 5 client."""
 
     __PKGNAME = 'hibernate'
 
@@ -20,57 +20,37 @@ class HibernatePackage(Package):
 
     def __init__(self):
         super(HibernatePackage, self).__init__(self.__PKGNAME)
-        self._hib3 = None
-        self._hib5 = None
+        self._hib = None
 
-        self.staged = [Stage(name='hibernate3',
-                             title='Hibernate3 Driver',
-                             requirements='Java 8 or 11'),
-
-                       Stage(name='hibernate5',
+        self.staged = [Stage(name='hibernate5',
                              title='Hibernate5 Driver',
                              requirements='Java 8 or 11')]
 
-        self.stage3 = self.staged[0]
-        self.stage5 = self.staged[1]
+        self.stage = self.staged[0]
 
     def download(self):
-        # Hibernate is complicated because both versions 3 and 5 are released
-        # in the same Maven repository.
         mvn = MavenMetadata(self.__PATH)
 
-        # Find the newest hib3 and hib5 versions
+        # Find the newest hib5 versions
         for ver in mvn.metadata.find('versioning/versions'):
-            if ver.text.endswith('hib3'):
-                self.stage3.version = ver.text
-            elif ver.text.endswith('hib5'):
-                self.stage5.version = ver.text
+            self.stage.version = ver.text
 
-        self._hib3 = Artifact(self.name, 'nuodb-hibernate-hib3.jar',
-                              '{}/{}/{}'.format(mvn.baseurl, self.stage3.version,
-                                                self.__JAR.format(self.stage3.version)))
-
-        self._hib5 = Artifact(self.name, 'nuodb-hibernate-hib5.jar',
+        self._hib = Artifact(self.name, 'nuodb-hibernate-hib5.jar',
                               '{}/{}/{}'.format(mvn.baseurl,
-                                                self.stage5.version, self.__JAR.format(self.stage5.version)))
+                                                self.stage.version, self.__JAR.format(self.stage.version)))
 
         # We only download the actual jar files
-        self._hib3.update()
-        self._hib5.update()
+        self._hib.update()
 
     def unpack(self):
         rmdir(self.pkgroot)
         mkdir(self.pkgroot)
-        copy(self._hib3.path, os.path.join(self.pkgroot, 'nuodb-hibernate-hib3.jar'))
-        copy(self._hib5.path, os.path.join(self.pkgroot, 'nuodb-hibernate-hib5.jar'))
+        copy(self._hib.path, os.path.join(self.pkgroot, 'nuodb-hibernate-hib5.jar'))
         savefile(os.path.join(self.pkgroot, 'LICENSE.txt'), self.getlicense('3BSD'))
 
     def install(self):
-        self.stage3.stage('jar', ['nuodb-hibernate-hib3.jar'])
-        self.stage3.stage('doc', ['LICENSE.txt'])
-
-        self.stage5.stage('jar', ['nuodb-hibernate-hib5.jar'])
-        self.stage5.stage('doc', ['LICENSE.txt'])
+        self.stage.stage('jar', ['nuodb-hibernate-hib5.jar'])
+        self.stage.stage('doc', ['LICENSE.txt'])
 
 
 # Create and register this package
